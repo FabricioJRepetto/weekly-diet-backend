a80dcd2ad874719fb8f0634e275bad4fe8207cd2
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-01-01 13:42:47 -0300

    dates equality fix

--- src/controllers/history_controller.js
+++ src/controllers/history_controller.js
@@ -22,7 +22,7 @@ const weekAnalist = (history, today, start) => {
 
             if (eDate >= Start && eDate <= Today) {
 
-                if (eDate === today) {
+                if (eDate.getTime() === today.getTime()) {
                     !aux.today && (aux.today = [])
                     aux.today.push(e)
                 }
